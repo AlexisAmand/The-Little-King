9b77b0e281c077fe3933e1bb41e338b169cb9126
Alex <alexis.amand@gmail.com>
2023-03-24 09:19:39 +0100

    [update] le script LevelSelector dans le dossier UI

---
+++ Assets/Scripts/UI/LevelSelector.cs
@@ -0,0 +1,13 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+
+    public void LoadLevelPassed(string levelname)
+    {
+        SceneManager.LoadScene(levelname);
+    }
+
+}

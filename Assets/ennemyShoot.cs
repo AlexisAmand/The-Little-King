42f00e83f0d4fc96c0e03311da0d3166800b58b3
Alex <alexis.amand@gmail.com>
2023-04-13 15:36:49 +0200

    [update] son quand la cible touche sa cible

--- Assets/ennemyShoot.cs
+++ Assets/ennemyShoot.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class ennemyShoot : MonoBehaviour
 {
+    public AudioClip sound;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Arrow"))
         {
+            // on joue le son
+            AudioManager.Instance.PlayClipAt(sound, transform.position);
+
+            // on détruit de gameobject de l'ennemi
             Destroy(collision.gameObject);
             Destroy(gameObject);
         }
